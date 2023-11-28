81c1d9b4115acc826aeb154992082e968acee21e
hsm7 <hsm.devonly@gmail.com>
2023-11-28 18:03:15 +0400

    Minor update.

--- src/main/scala/io/github/hsm7/jobs/Application.scala
+++ src/main/scala/io/github/hsm7/jobs/Application.scala
@@ -4,16 +4,16 @@ import cats.effect.{IO, IOApp}
 import org.http4s.ember.server.EmberServerBuilder
 import pureconfig.ConfigSource
 
-import io.github.hsm7.jobs.config.EmberConfig
 import io.github.hsm7.jobs.config.syntax.*
-import io.github.hsm7.jobs.http.routes.HealthRoutes
+import io.github.hsm7.jobs.config.EmberConfig
+import io.github.hsm7.jobs.http.HttpApi
 
 object Application extends IOApp.Simple {
 
   override def run: IO[Unit] = ConfigSource.default.loadF[IO, EmberConfig].flatMap { emberConfig =>
     EmberServerBuilder
       .default[IO]
-      .withHttpApp(HealthRoutes[IO].routes.orNotFound)
+      .withHttpApp(HttpApi[IO].routes.orNotFound)
       .withHost(emberConfig.host)
       .withPort(emberConfig.port)
       .build
---
+++ src/main/scala/io/github/hsm7/jobs/config/syntax/package.scala
@@ -0,0 +1,15 @@
+package io.github.hsm7.jobs.config.syntax
+
+import cats.MonadThrow
+import cats.implicits.*
+import pureconfig.error.ConfigReaderException
+import pureconfig.{ConfigReader, ConfigSource}
+
+import scala.reflect.ClassTag
+
+extension (source: ConfigSource)
+  def loadF[F[_], A](using reader: ConfigReader[A], F: MonadThrow[F], tag: ClassTag[A]): F[A] =
+    F.pure(source.load[A]).flatMap {
+      case Left(errors) => F.raiseError[A](ConfigReaderException(errors))
+      case Right(value) => F.pure(value)
+    }
